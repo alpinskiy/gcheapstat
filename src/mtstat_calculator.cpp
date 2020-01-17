afca890a622a8caa28d5da1f251272534ed35961
Mikhail Alpinskiy <malpinskiy@gmail.com>
2020-01-18 00:44:39 +0200

    Segment walking code refactoring

--- src/mtstat_calculator.cpp
+++ src/mtstat_calculator.cpp
@@ -10,34 +10,8 @@ HRESULT MtStatCalculator::Initialize(HANDLE hprocess,
                                      ISOSDacInterface* sos_dac_interface) {
   hprocess_ = hprocess;
   sos_dac_interface_ = sos_dac_interface;
-  info_.Request(sos_dac_interface_.get());
-  sos_dac_interface_->GetUsefulGlobals(&useful_globals_);
-  //  Allocation contexts
-  DacpThreadStoreData threadstore_data{};
-  auto hr = threadstore_data.Request(sos_dac_interface_.get());
-  if (SUCCEEDED(hr)) {
-    DacpThreadData thread_data{};
-    allocation_contexts_.reserve(threadstore_data.threadCount);
-    for (auto thread = threadstore_data.firstThread; thread;
-         thread = thread_data.nextThread) {
-      hr = thread_data.Request(sos_dac_interface_.get(), thread);
-      if (FAILED(hr)) {
-        LogError(L"Error getting ThreadData at 0x%016" PRIX64
-                 ", code 0x%08lx\n",
-                 thread, hr);
-      } else if (thread_data.allocContextPtr) {
-        auto i = 0u;
-        for (; i < allocation_contexts_.size() &&
-               allocation_contexts_[i].ptr != thread_data.allocContextPtr;
-             ++i)
-          ;
-        if (i == allocation_contexts_.size())
-          allocation_contexts_.push_back(
-              {static_cast<uintptr_t>(thread_data.allocContextPtr),
-               static_cast<uintptr_t>(thread_data.allocContextLimit)});
-      }
-    }
-  }
+  auto hr = info_.Request(sos_dac_interface_.get());
+  hr = sos_dac_interface_->GetUsefulGlobals(&useful_globals_);
   // Heaps
   if (info_.bServerMode) {
     std::vector<CLRDATA_ADDRESS> heap_addr_list(info_.HeapCount);
@@ -72,7 +46,7 @@ HRESULT MtStatCalculator::Initialize(HANDLE hprocess,
     } else
       heaps_.push_back(heap);
   }
-  // Segments
+  // Segments & gen0 allocation contexts
   for (size_t i = 0; i < heaps_.size(); ++i) {
     auto heap = &heaps_[i];
     if (heap->generation_table[0].allocContextPtr) {
@@ -106,10 +80,45 @@ HRESULT MtStatCalculator::Initialize(HANDLE hprocess,
       addr = segment.data.next;
     }
   }
+  //  Thread allocation contexts
+  DacpThreadStoreData threadstore_data{};
+  hr = threadstore_data.Request(sos_dac_interface_.get());
   if (SUCCEEDED(hr)) {
-    std::sort(allocation_contexts_.begin(), allocation_contexts_.end(),
-              [](auto& a, auto& b) { return a.ptr < b.ptr; });
+    DacpThreadData thread_data{};
+    for (auto thread = threadstore_data.firstThread; thread;
+         thread = thread_data.nextThread) {
+      hr = thread_data.Request(sos_dac_interface_.get(), thread);
+      if (FAILED(hr)) {
+        LogError(L"Error getting ThreadData at 0x%016" PRIX64
+                 ", code 0x%08lx\n",
+                 thread, hr);
+      } else if (thread_data.allocContextPtr) {
+        auto i = 0u;
+        for (; i < allocation_contexts_.size() &&
+               allocation_contexts_[i].ptr != thread_data.allocContextPtr;
+             ++i)
+          ;
+        if (i == allocation_contexts_.size())
+          allocation_contexts_.push_back(
+              {static_cast<uintptr_t>(thread_data.allocContextPtr),
+               static_cast<uintptr_t>(thread_data.allocContextLimit)});
+      }
+    }
   }
+  for (auto it = allocation_contexts_.begin();
+       it != allocation_contexts_.end();) {
+    if (it->ptr < it->limit) {
+      ++it;
+      continue;
+    }
+    if (it->ptr == it->limit)
+      LogError(L"Empty allocation context encountered\n");
+    else
+      LogError(L"Invalid allocation context encountered\n");
+    it = allocation_contexts_.erase(it);
+  }
+  std::sort(allocation_contexts_.begin(), allocation_contexts_.end(),
+            [](auto& a, auto& b) { return a.ptr < b.ptr; });
   return hr;
 }
 
@@ -117,13 +126,16 @@ HRESULT MtStatCalculator::Calculate(std::vector<MtStat>& mtstat) {
   std::unordered_map<uintptr_t, MtAddrStat>{}.swap(dict_);
   for (auto& segment : segments_[0]) {
     if (segment.addr == segment.heap->ephemeral_heap_segment)
-      WalkEphemeralHeapSegment(segment);
+      WalkSegment<kAlignment>(segment.data.mem, segment.heap->alloc_allocated,
+                              L"ephemeral");
     else
-      WalkSmallObjectHeapSegment(segment);
+      WalkSegment<kAlignment>(segment.data.mem, segment.data.allocated,
+                              L"small object");
     if (IsCancelled()) return S_FALSE;
   }
   for (auto& segment : segments_[1]) {
-    WalkLargeObjectHeapSegment(segment);
+    WalkSegment<kAlignmentLarge>(segment.data.mem, segment.data.allocated,
+                                 L"large object");
     if (IsCancelled()) return S_FALSE;
   }
   std::vector<MtStat> ret;
@@ -139,251 +151,6 @@ HRESULT MtStatCalculator::Calculate(std::vector<MtStat>& mtstat) {
   std::swap(ret, mtstat);
   return S_OK;
 }
-
-// clang-format off
-// Three functions below are largerly the same but separated
-// because this way it is easier to write custom error messages
-// for each kind of segment (small, large and ephemeral).
-// Automatic formatting is turned off to keep line structure the same,
-// so you can copy and past any of two to the comparer and see
-// they differ pretty much in trace messages only
-// + WalkEphemeralHeapSegment have to check allocation context.
-
-HRESULT MtStatCalculator::WalkSmallObjectHeapSegment(Segment& segment) {
-  auto segment_first = static_cast<uintptr_t>(segment.data.mem);
-  auto segment_last = static_cast<uintptr_t>(segment.data.allocated);
-  if (segment_last < segment_first) {
-    LogError(L"Invalid segment range encountered, small object heap\n");
-    return E_FAIL;
-  }
-  auto segment_size = static_cast<size_t>(segment_last - segment_first);
-  if (!segment_size) {
-    LogError(L"Empty segment encountered, small object heap\n");
-    return S_FALSE;
-  }
-  std::vector<BYTE> buffer(segment_size);
-  SIZE_T read = 0;
-  if (!ReadProcessMemory(hprocess_, reinterpret_cast<LPCVOID>(segment_first),
-                         &buffer[0], segment_size, &read)) {
-    auto hr = HRESULT_FROM_WIN32(GetLastError());
-    LogError(
-        L"Error reading segment memory, code 0x%08lx, small object heap\n",
-        segment_size, read);
-    return hr;
-  }
-  if (read != segment_size) {
-    LogError(
-        L"Incomplete segment memory read, bytes requested %zu, read %zu, small object heap\n",
-        segment_size, read);
-    return E_FAIL;
-  }
-  auto buffer_first = &buffer[0];
-  auto buffer_ptr = buffer_first;
-  auto buffer_bytes_left = segment_size;
-  for (; kMinObjectSize <= buffer_bytes_left;) {
-    if (IsCancelled()) return S_FALSE;
-    auto mt = *reinterpret_cast<uintptr_t*>(buffer_ptr) & ~3;
-    if (!mt) {
-      LogError(
-          L"Zero method table address encountered, %zu bytes left, small object heap\n",
-          buffer_bytes_left);
-      return E_FAIL;
-    }
-    MtAddrStat* stat;
-    auto hr = GetMtAddrStat(mt, &stat);
-    if (FAILED(hr)) {
-      LogError(
-          L"Error getting method table data, code 0x%08lx, small object heap\n",
-          hr);
-      return hr;
-    }
-    auto component_count =
-        *reinterpret_cast<PDWORD>(buffer_ptr + sizeof(uintptr_t));
-    auto object_size = UpdateMtAddrStat(mt, component_count, stat);
-    auto object_size_aligned = Align<kAlignment>(object_size);
-    if (buffer_bytes_left < object_size_aligned ||
-        object_size_aligned <= sizeof(uintptr_t)) {
-      LogError(
-          L"Object size %zu is out of valid range, size aligned %zu, %zu bytes left, small object heap\n",
-          object_size, object_size_aligned + sizeof(uintptr_t),
-          buffer_bytes_left + sizeof(uintptr_t));
-      return E_FAIL;
-    }
-    buffer_ptr += object_size_aligned;
-    buffer_bytes_left -= object_size_aligned;
-  }
-  if (buffer_bytes_left) {
-    LogError(
-      L"Objects total size isn't equal to segment size %zu, %zu bytes left, small object heap",
-      segment_size, buffer_bytes_left);
-    return E_FAIL;
-  }
-  return S_OK;
-}
-
-HRESULT MtStatCalculator::WalkEphemeralHeapSegment(Segment& segment) {
-  auto segment_first = static_cast<uintptr_t>(segment.data.mem);
-  auto segment_last = static_cast<uintptr_t>(segment.heap->alloc_allocated);
-  if (segment_last < segment_first) {
-    LogError(L"Invalid segment range encountered, ephemeral segment\n");
-    return E_FAIL;
-  }
-  auto segment_size = static_cast<size_t>(segment_last - segment_first);
-  if (!segment_size) {
-    LogError(L"Empty segment encountered, ephemeral segment\n");
-    return S_FALSE;
-  }
-  std::vector<BYTE> buffer(segment_size);
-  SIZE_T read = 0;
-  if (!ReadProcessMemory(hprocess_, reinterpret_cast<LPCVOID>(segment_first),
-                         &buffer[0], segment_size, &read)) {
-    auto hr = HRESULT_FROM_WIN32(GetLastError());
-    LogError(
-      L"Error reading segment memory, code 0x%08lx, ephemeral segment\n",
-      segment_size, read);
-    return hr;
-  }
-  if (read != segment_size) {
-    LogError(
-        L"Incomplete segment memory read, bytes requested %zu, read %zu, ephemeral segment\n",
-        segment_size, read);
-    return E_FAIL;
-  }
-  auto segment_ptr = segment_first;
-  auto allocation_context =
-      std::find_if(allocation_contexts_.cbegin(), allocation_contexts_.cend(),
-                   [segment_ptr](auto& a) { return segment_ptr < a.ptr; });
-  auto buffer_first = &buffer[0];
-  auto buffer_last = buffer_first + segment_size;
-  auto buffer_ptr = buffer_first;
-  auto buffer_bytes_left = segment_size;
-  for (; kMinObjectSize <= buffer_bytes_left;) {
-    if (IsCancelled()) return S_FALSE;
-    // Is this the beginning of an allocation context?
-    if (allocation_context != allocation_contexts_.cend() && segment_ptr == allocation_context->ptr) {
-      segment_ptr = allocation_context->limit + Align<kAlignment>(kMinObjectSize);
-      if (segment_last < segment_ptr) {
-        LogError(L"Allocation context limit is out of ephemeral segment end\n");
-        return E_FAIL;
-      }
-      auto offset = segment_ptr - segment_first;
-      buffer_ptr = buffer_first + offset;
-      buffer_bytes_left = buffer_last - buffer_ptr;
-      ++allocation_context;
-      continue;
-    }
-    auto mt = *reinterpret_cast<uintptr_t*>(buffer_ptr) & ~3;
-    if (!mt) {
-      LogError(
-          L"Zero method table address encountered, %zu bytes left, ephemeral segment\n",
-          buffer_bytes_left);
-      return E_FAIL;
-    }
-    MtAddrStat* stat;
-    auto hr = GetMtAddrStat(mt, &stat);
-    if (FAILED(hr)) {
-      LogError(
-          L"Error getting method table data, code 0x%08lx, ephemeral segment\n",
-          hr);
-      return hr;
-    }
-    auto component_count =
-        *reinterpret_cast<PDWORD>(buffer_ptr + sizeof(uintptr_t));
-    auto object_size = UpdateMtAddrStat(mt, component_count, stat);
-    auto object_size_aligned = Align<kAlignment>(object_size);
-    if (buffer_bytes_left < object_size_aligned ||
-        object_size_aligned <= sizeof(uintptr_t)) {
-      LogError(
-          L"Object size %zu is out of valid range, size aligned %zu, %zu bytes left, ephemeral segment\n",
-          object_size, object_size_aligned + sizeof(uintptr_t),
-          buffer_bytes_left + sizeof(uintptr_t));
-      return E_FAIL;
-    }
-    buffer_ptr += object_size_aligned;
-    buffer_bytes_left -= object_size_aligned;
-    segment_ptr += object_size_aligned;
-  }
-  if (buffer_bytes_left) {
-    LogError(
-        L"Objects total size isn't equal to segment size %zu, %zu bytes left, ephemeral segment",
-        segment_size, buffer_bytes_left);
-    return E_FAIL;
-  }
-  return S_OK;
-}
-
-HRESULT MtStatCalculator::WalkLargeObjectHeapSegment(Segment& segment) {
-  auto segment_first = static_cast<uintptr_t>(segment.data.mem);
-  auto segment_last = static_cast<uintptr_t>(segment.data.allocated);
-  if (segment_last < segment_first) {
-    LogError(L"Invalid segment range encountered, large object heap\n");
-    return E_FAIL;
-  }
-  auto segment_size = static_cast<size_t>(segment_last - segment_first);
-  if (!segment_size) {
-    LogError(L"Empty segment encountered, large object heap\n");
-    return S_FALSE;
-  }
-  std::vector<BYTE> buffer(segment_size);
-  SIZE_T read = 0;
-  if (!ReadProcessMemory(hprocess_, reinterpret_cast<LPCVOID>(segment_first),
-                         &buffer[0], segment_size, &read)) {
-    auto hr = HRESULT_FROM_WIN32(GetLastError());
-    LogError(
-        L"Error reading segment memory, code 0x%08lx, large object heap\n",
-        segment_size, read);
-    return hr;
-  }
-  if (read != segment_size) {
-    LogError(
-        L"Incomplete segment memory read, bytes requested %zu, read %zu, large object heap\n",
-        segment_size, read);
-    return E_FAIL;
-  }
-  auto buffer_first = &buffer[0];
-  auto buffer_ptr = buffer_first;
-  auto buffer_bytes_left = segment_size;
-  for (; kMinObjectSize <= buffer_bytes_left;) {
-    if (IsCancelled()) return S_FALSE;
-    auto mt = *reinterpret_cast<uintptr_t*>(buffer_ptr) & ~3;
-    if (!mt) {
-      LogError(
-          L"Zero method table address encountered, %zu bytes left, large object heap\n",
-          buffer_bytes_left);
-      return E_FAIL;
-    }
-    MtAddrStat* stat;
-    auto hr = GetMtAddrStat(mt, &stat);
-    if (FAILED(hr)) {
-      LogError(
-          L"Error getting method table data, code 0x%08lx, large object heap\n",
-          hr);
-      return hr;
-    }
-    auto component_count =
-        *reinterpret_cast<PDWORD>(buffer_ptr + sizeof(uintptr_t));
-    auto object_size = UpdateMtAddrStat(mt, component_count, stat);
-    auto object_size_aligned = Align<kAlignmentLarge>(object_size);
-    if (buffer_bytes_left < object_size_aligned ||
-        object_size_aligned <= sizeof(uintptr_t)) {
-      LogError(
-          L"Object size %zu is out of valid range, size aligned %zu, %zu bytes left, large object heap\n",
-          object_size, object_size_aligned + sizeof(uintptr_t),
-          buffer_bytes_left + sizeof(uintptr_t));
-      return E_FAIL;
-    }
-    buffer_ptr += object_size_aligned;
-    buffer_bytes_left -= object_size_aligned;
-  }
-  if (buffer_bytes_left) {
-    LogError(
-      L"Objects total size isn't equal to segment size %zu, %zu bytes left, large object heap",
-      segment_size, buffer_bytes_left);
-    return E_FAIL;
-  }
-  return S_OK;
-}
-// clang-format on
 
 HRESULT CalculateMtStat(HANDLE hprocess, ISOSDacInterface* sos_dac_interface,
                         std::vector<MtStat>& mtstat) {
--- src/mtstat_calculator.h
+++ src/mtstat_calculator.h
@@ -37,47 +37,137 @@ class MtStatCalculator {
     size_t size_total;
   };
 
-  HRESULT WalkSmallObjectHeapSegment(Segment& segment);
-  HRESULT WalkEphemeralHeapSegment(Segment& segment);
-  HRESULT WalkLargeObjectHeapSegment(Segment& segment);
+  template <size_t Alignment>
+  inline void WalkSegment(CLRDATA_ADDRESS mem, CLRDATA_ADDRESS allocated,
+                          PCWSTR name) {
+    WalkSegment<Alignment>(static_cast<uintptr_t>(mem),
+                           static_cast<uintptr_t>(allocated), name);
+  }
+
+  template <size_t Alignment>
+  void WalkSegment(uintptr_t mem, uintptr_t allocated, PCWSTR name) {
+    if (allocated < mem) {
+      LogError(L"Invalid segment range encountered, %s segment\n", name);
+      return;
+    }
+    auto size = static_cast<size_t>(allocated - mem);
+    if (!size) {
+      LogError(L"Empty segment encountered, %s segment\n", name);
+      return;
+    }
+    std::vector<BYTE> buffer(size);
+    SIZE_T read = 0;
+    if (!ReadProcessMemory(hprocess_, reinterpret_cast<LPCVOID>(mem),
+                           &buffer[0], size, &read)) {
+      auto hr = HRESULT_FROM_WIN32(GetLastError());
+      LogError(L"Error reading segment memory, code 0x%08lx, %s segment\n", hr,
+               name);
+      return;
+    }
+    if (read != size) {
+      LogError(
+          L"Incomplete segment memory read, bytes requested %zu, read %zu, "
+          L"%s segment\n",
+          size, read, name);
+      return;
+    }
+    PBYTE ptr = &buffer[0], end = &buffer[0] + size;
+    for (auto it = std::find_if(allocation_contexts_.cbegin(),
+                                allocation_contexts_.cend(),
+                                [mem, allocated](auto& a) {
+                                  return mem <= a.ptr && a.ptr < allocated;
+                                });
+         it != allocation_contexts_.cend(); ++it) {
+      WalkMemory<Alignment>(ptr, (std::min)(it->ptr, allocated) - mem, name);
+      if (allocated < it->limit) {
+        LogError(
+            L"Allocation context limit goes beyond %s segment boundaries\n",
+            name);
+        return;
+      }
+      auto limit = it->limit + Align<kAlignment>(kMinObjectSize);
+      if (allocated < limit) {
+        LogError(
+            L"Aligned allocation context limit goes beyond %s segment "
+            L"boundaries\n",
+            name);
+        return;
+      }
+      size = allocated - limit;
+      ptr = end - size;
+    }
+    WalkMemory<Alignment>(ptr, size, name);
+  }
+
+  template <size_t Alignment>
+  void WalkMemory(PBYTE ptr, size_t size, PCWSTR name) {
+    for (size_t object_size; kMinObjectSize <= size;
+         ptr += object_size, size -= object_size) {
+      if (IsCancelled()) return;
+      // Get method table address
+      auto mt = *reinterpret_cast<uintptr_t*>(ptr) & ~3;
+      if (!mt) {
+        LogError(
+            L"Zero method table address encountered, skip %zu bytes, %s "
+            L"segment\n",
+            size, name);
+        return;
+      }
+      // Get method table data
+      auto it = dict_.find(mt);
+      if (it == dict_.end()) {
+        DacpMethodTableData mt_data{};
+        auto hr = mt_data.Request(sos_dac_interface_.get(), mt);
+        if (FAILED(hr)) {
+          LogError(
+              L"Error getting method table data, code 0x%08lx, skip %zu "
+              L"bytes, %s segment\n",
+              hr, name);
+          return;
+        }
+        MtAddrStat stat{mt_data.BaseSize, mt_data.ComponentSize};
+        it = dict_.emplace(mt, stat).first;
+      }
+      // Calculate object size
+      auto& stat = it->second;
+      auto component_count = *reinterpret_cast<PDWORD>(ptr + sizeof(uintptr_t));
+      if (mt == useful_globals_.StringMethodTable) {
+        // The component size on a String does not contain the trailing NULL
+        // character, so we must add that ourselves.
+        ++component_count;
+      }
+      object_size = stat.sizeof_base + component_count * stat.sizeof_component;
+#if _WIN64
+      if (object_size < kMinObjectSize) object_size = kMinObjectSize;
+#endif
+      // Validate object size
+      if (!object_size || size < object_size) {
+        LogError(
+            L"Object size %zu is out of valid range, skip %zu bytes, %s "
+            L"segment\n",
+            object_size, size, name);
+        return;
+      }
+      // Update statistics
+      ++stat.count;
+      stat.size_total += object_size;
+      // Align object size
+      object_size = Align<Alignment>(object_size);
+      if (!object_size || size < object_size) {
+        LogError(
+            L"Aligned object size is out of valid range, value %zu, skip %zu "
+            L"bytes, %s segment\n",
+            object_size, size, name);
+        return;
+      }
+    }
+    if (size) LogError(L"Skip %zu bytes of %s segment", size, name);
+  }
 
   template <size_t Alignment>
   inline uintptr_t Align(uintptr_t v) {
     auto constexpr kAlign = Alignment - 1;
     return (v + kAlign) & ~kAlign;
-  }
-
-  inline HRESULT GetMtAddrStat(uintptr_t mt, MtAddrStat** ptr) {
-    auto it = dict_.find(mt);
-    if (it != dict_.end()) {
-      *ptr = &it->second;
-    } else {
-      DacpMethodTableData mt_data{};
-      auto hr = mt_data.Request(sos_dac_interface_.get(), mt);
-      if (FAILED(hr)) return hr;
-      MtAddrStat stat{mt_data.BaseSize, mt_data.ComponentSize};
-      *ptr = &dict_.emplace(mt, stat).first->second;
-    }
-    return S_OK;
-  }
-
-  inline size_t UpdateMtAddrStat(uintptr_t mt, DWORD component_count,
-                                 MtAddrStat* stat) {
-    // Calculate object size
-    if (mt == useful_globals_.StringMethodTable) {
-      // The component size on a String does not contain the trailing NULL
-      // character, so we must add that ourselves.
-      ++component_count;
-    }
-    auto object_size =
-        stat->sizeof_base + component_count * stat->sizeof_component;
-#if _WIN64
-    if (object_size < kMinObjectSize) object_size = kMinObjectSize;
-#endif
-    // Update statistics
-    ++stat->count;
-    stat->size_total += object_size;
-    return object_size;
   }
 
   HANDLE hprocess_;
