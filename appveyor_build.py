4df88342c968d53742f57caee6de8db77230877a
Mikhail Alpinskiy <malpinskiy@metaquotes.net>
2020-01-26 21:09:18 +0200

    Run appveyor with build script

---
+++ appveyor_build.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import zipfile
+import subprocess
+
+def zip_binary(platform, configuration):
+  if platform == 'x86':
+    name = 'gcheapstat32.exe'
+  elif platform == "x64":
+    name = 'gcheapstat64.exe'
+  else:
+    sys.exit('Expected platform either x86 or x64')
+  path = os.path.join("out", platform, configuration, "gcheapstat.exe")
+  zipf = zipfile.ZipFile('gcheapstat.zip', 'a', zipfile.ZIP_DEFLATED)
+  zipf.write(path, name)
+  zipf.close()
+
+def build(platform, configuration):
+  subprocess.call(['msbuild',
+    'gcheapstat.sln',
+    '/p:Platform=' + platform,
+    '/p:Configuration=' + configuration,
+    '/l:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"'])
+
+if __name__ == '__main__':
+  build('x86', 'Release')
+  build('x64', 'Release')
+  zip_binary('x86', 'Release')
+  zip_binary('x64', 'Release')
