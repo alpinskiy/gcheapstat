d52ee6156d3a0256daec4e84201794205d5d9fea
Mikhail Alpinskiy <malpinskiy@metaquotes.net>
2020-01-26 21:48:08 +0200

    Push artifact from script

--- appveyor_build.py
+++ appveyor_build.py
@@ -3,7 +3,7 @@ import sys
 import zipfile
 import subprocess
 
-def zip_binary(platform, configuration):
+def zip_binary(zipname, platform, configuration):
   if platform == 'x86':
     name = 'gcheapstat32.exe'
   elif platform == "x64":
@@ -11,10 +11,10 @@ def zip_binary(platform, configuration):
   else:
     sys.exit('Expected platform either x86 or x64')
   path = os.path.join("out", platform, configuration, "gcheapstat.exe")
-  tag = os.getenv('APPVEYOR_REPO_TAG_NAME') or ''
-  zipf = zipfile.ZipFile('gcheapstat' + tag + '.zip', 'a', zipfile.ZIP_DEFLATED)
+  zipf = zipfile.ZipFile(zipname, 'a', zipfile.ZIP_DEFLATED)
   zipf.write(path, name)
   zipf.close()
+  return zipname
 
 def build(platform, configuration):
   subprocess.call(['msbuild',
@@ -23,9 +23,15 @@ def build(platform, configuration):
     '/p:Configuration=' + configuration,
     '/l:C:\\Program Files\\AppVeyor\\BuildAgent\\Appveyor.MSBuildLogger.dll'])
 
+def push_artifact(name):
+  subprocess.call(['appveyor', 'PushArtifact', name])
+
 if __name__ == '__main__':
   print(sys.argv)
   build('x86', 'Release')
   build('x64', 'Release')
-  zip_binary('x86', 'Release')
-  zip_binary('x64', 'Release')
+  tag = os.getenv('APPVEYOR_REPO_TAG_NAME') or ''
+  zipname = 'gcheapstat' + tag + '.zip';
+  zip_binary(zipname, 'x86', 'Release')
+  zip_binary(zipname, 'x64', 'Release')
+  push_artifact(zipname)
