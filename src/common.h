c45ceaf59e9e598a8dc77732ba6d853e88ae249c
Mikhail Alpinskiy <malpinskiy@gmail.com>
2020-02-08 22:50:35 +0200

    Fix PLHxPTR format specifier

--- src/common.h
+++ src/common.h
@@ -2,7 +2,7 @@
 
 auto constexpr kPipeNameFormat = L"\\pipe\\gcheapstat%" PRIu32;
 #ifdef _WIN64
-#define PLHxPTR "0x016" PRIxPTR
+#define PLHxPTR "0x%016"##PRIxPTR
 #else
-#define PLHxPTR "0x08" PRIxPTR
+#define PLHxPTR "0x%08"##PRIxPTR
 #endif
